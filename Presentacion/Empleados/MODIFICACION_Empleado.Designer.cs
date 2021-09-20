edb403c55335b18a06fe7ca408c7e2a19f3a94c2
Stephan Yanguez <andre97mil@hotmail.com>
2021-09-20 14:57:23 -0300

    prueb

--- Presentacion/Empleados/MODIFICACION_Empleado.Designer.cs
+++ Presentacion/Empleados/MODIFICACION_Empleado.Designer.cs
@@ -29,19 +29,34 @@ namespace Vivero.Presentacion.Empleados
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MODIFICACION_Empleado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "MODIFICACION_Empleado";
             this.Text = "Modificacion empleado";
+            this.Load += new System.EventHandler(this.MODIFICACION_Empleado_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
